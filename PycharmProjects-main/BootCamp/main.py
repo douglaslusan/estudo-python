10aaddbcf15c6220aa88364af79c76ee6afa2fa1
Douglas Luiz dos Santos <douglas.lusan@gmail.com>
2022-05-01 00:16:28 -0300

    implementado ganhou

--- PycharmProjects-main/BootCamp/main.py
+++ PycharmProjects-main/BootCamp/main.py
@@ -4,22 +4,29 @@ palavras = ['airam', 'douglas', 'aline', 'comida']
 letraAdvinhada = list()
 letrasJaChutadas = list()
 
+ganhou = 7
+
 palavraEscolhida = random.choice(palavras)
 
 for i in range(len(palavraEscolhida)):
 	letraAdvinhada.append('_ ')
 print(palavraEscolhida)
-print(letraAdvinhada)
-while True:
+print(''.join(letraAdvinhada))
 
+while ganhou:
 	letraEscolhida = input('\nDIGITE UMA LETRA: ')
 	letrasJaChutadas.append(letraEscolhida)
 	print(f'Voce ja Chutou as letras:\n{letrasJaChutadas}\n\n')
-
 
 	for i in range(len(palavraEscolhida)):
 		if letraEscolhida == palavraEscolhida[i]:
 			letraAdvinhada[i] = letraEscolhida
 
 
-	print(letraAdvinhada)
+	for i in letraAdvinhada:
+		if '_ ' not in letraAdvinhada:
+			ganhou = 0
+			print('ganhou')
+			break
+
+	print(' '.join(letraAdvinhada))
