cafdb8fdd01226871bd49c05bb8d33ec76d47b62
Douglas Luiz dos Santos <douglas.lusan@gmail.com>
2022-05-02 21:35:11 -0300

    deixando mais bonito

---
+++ PycharmProjects-main/BootCamp/hangman.py
@@ -0,0 +1,134 @@
+import random
+import palavras_hangman
+
+def desenho(vidas):
+	if vidas == 6:
+		print('''
+	  _______
+     |/      |
+     |      
+     |      
+     |      
+     |      
+     |
+    _|___
+	''')
+
+	elif vidas == 5:
+		print('''
+	  _______
+     |/      |
+     |      (_)
+     |      
+     |      
+     |      
+     |
+    _|___
+	''')
+	elif vidas == 4:
+		print('''
+	  _______
+     |/      |
+     |      (_)
+     |       |
+     |       |
+     |      
+     |
+    _|___
+	''')
+	elif vidas == 3:
+		print('''
+	  _______
+     |/      |
+     |      (_)
+     |       |/
+     |       |
+     |      
+     |
+    _|___
+	''')
+	elif vidas == 2:
+		print('''
+	  _______
+	 |/      |
+	 |      (_)
+	 |      \|/
+	 |       |
+	 |      
+	 |
+	_|___
+	''')
+	elif vidas == 1:
+		print('''
+	  _______
+	 |/      |
+	 |      (_)
+	 |      \|/
+	 |       |
+	 |      /
+	 |
+	_|___
+	''')
+	else:
+		print('''
+	  _______
+	 |/      |
+	 |      (_)
+	 |      \|/
+	 |       |
+	 |      / \\
+	 |
+	_|___
+	''')
+
+
+
+letraAdvinhada = list()
+letrasJaChutadas = list()
+
+ganhou = 6
+
+palavraEscolhida = random.choice(palavras_hangman.lista)
+tamanho = len(palavraEscolhida)
+
+for i in palavraEscolhida:
+	letraAdvinhada.append('_ ')
+print(''.join(letraAdvinhada))
+
+while ganhou:
+
+	desenho(ganhou)
+
+	while True:  # verificacao de letras digitadas
+		letraEscolhida = input('\nDIGITE UMA LETRA: ').lower()
+		if letraEscolhida.isalpha():
+			if letraEscolhida not in letrasJaChutadas:
+				letrasJaChutadas.append(letraEscolhida)
+				break
+			else:
+				print('Voce ja chutou esta letra')
+		else:
+			print('Digite somente LETRAS')
+
+
+	print(f'Voce ja Chutou as letras:\n{letrasJaChutadas}\n\n')
+
+	for i in range(tamanho):
+		if letraEscolhida == palavraEscolhida[i]:
+			letraAdvinhada[i] = letraEscolhida
+
+	if letraEscolhida not in palavraEscolhida:
+		ganhou -=1
+		if ganhou == 0:
+			print('\033[1;31mVOCÊ PERDEU, MAIS SORTE DA PRÓXIMA VEZ\033[0;0m')
+			break
+
+	if '_ ' not in letraAdvinhada:
+		ganhou = 0
+		print('\033[32mVOCÊ GANHOU, PARABÉNS')
+		print(f'a palavra foi "* {palavraEscolhida.upper()} *"\033[0;0m')
+		break
+
+	print(f'Voce tem {ganhou} chances')
+
+	print(' '.join(letraAdvinhada))
---
+++ PycharmProjects-main/BootCamp/palavras_hangman.py
@@ -0,0 +1,4 @@
+lista = ['afobado', 'amendoim', 'banheiro', 'caatinga', 'cachorro', 'campeonato', 'capricornio', 'catapora', 'amarelo',
+		 'amiga', 'amor', 'ave', 'bolo', 'branco', 'cama', 'caneca', 'celular', 'clube', 'copo', 'doce', 'elefante',
+		 'escola', 'estojo', 'faca', 'foto', 'garfo', 'geleia', 'girafa', 'janela', 'limonada', 'meia', 'noite', 'ovo',
+		 'pai', 'parque', 'passarinho', 'peixe', 'pijama', 'rato', 'umbigo']
