6f7d629154783cda22609d5767b02004770623d4
Douglas Luiz dos Santos <douglas.lusan@gmail.com>
2022-05-28 22:07:02 -0300

    feito desafio ciphercezar

--- PycharmProjects-main/BootCamp/oitavoDia/cipherCezar.py
+++ PycharmProjects-main/BootCamp/oitavoDia/cipherCezar.py
@@ -1,4 +1,4 @@
-
+modo = input('digite [c] para criptografar ou [d] para descriptografar: ')
 frase = input('digite uma frase ').lower()
 
 palavraShift = [ord(asc) for asc in frase]
@@ -7,19 +7,18 @@ shift = int(input('digite quantas casas quer modificar as letras: '))
 
 frasePronta = ''
 
-for i in range(len(palavraShift)):
-	if palavraShift[i] == 32:
-		continue
-	palavraShift[i] += shift
-	if palavraShift[i] > 122:
-		palavraShift[i] -= 26
-	palavraCodificada = [chr(num) for num in palavraShift]
-	frasePronta = ''.join(palavraCodificada)
+if modo == 'c':
+	for i in range(len(palavraShift)):
+		if palavraShift[i] == 32:
+			continue
+		palavraShift[i] += shift
+		if palavraShift[i] > 122:
+			palavraShift[i] -= 26
+		palavraCodificada = [chr(num) for num in palavraShift]
+		frasePronta = ''.join(palavraCodificada)
 
-print(frasePronta)
-opcao = int(input('digite 1 para descodificar ou 0 para sair '))
-shift = int(input('digite quantas casas quer modificar as letras: '))
-if opcao == 1:
+	print(frasePronta)
+else:
 	for i in range(len(palavraShift)):
 		if palavraShift[i] == 32:
 			continue
@@ -30,7 +29,5 @@ if opcao == 1:
 		frasePronta = ''.join(palavraCodificada)
 
 	print(frasePronta)
-else:
-	print('FIM DO PROGRAMA')
 
 
