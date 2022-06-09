fa380e43818b1de35fc6369908094173b4a3cf95
Douglas Luiz dos Santos <douglas.lusan@gmail.com>
2022-06-09 19:36:55 -0300

    finalizando o leilao

--- PycharmProjects-main/BootCamp/diaNove/leilao.py
+++ PycharmProjects-main/BootCamp/diaNove/leilao.py
@@ -1,16 +1,45 @@
-cliente = {}
-listaClientes = []
+def titulo():
+	print('*-'*30)
+	print(f'{"COMEÇANDO O LEILÃO":^60}')
+	print('*-' * 30)
 
 
+def achar_maximo(clientes):
+	max_key = max(clientes, key=lambda key: clientes[key])
+	print(f'O vencedor é: {max_key} que ofertou valor de ${clientes[max_key]}')
+
+def logo():
+	print('''
+						 ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                         `'-------'`
+                       .-------------.
+                      /_______________\\
+	
+	''')
+
+clientes = {}
+
+titulo()
 while True:
-	nome = input('digite seu Nome: ')
-	valor = float(input('qual o valor a ser ofertado $'))
-	opcao = input('quer continuar? [S/N] '). upper()
+	logo()
+	nome = input('digite seu Nome para dar um lance: ').title()
+	valor = float(input('qual o valor do lance $'))
 
-	cliente[nome] = valor
+	clientes[nome] = valor
+
+	opcao = input('quer continuar? [S/N] ').upper()
 
 	if opcao == 'S':
-		print('\n' * 10)
+		print('\n')
 	else:
-		print(cliente)
+		achar_maximo(clientes)
 		break
+
