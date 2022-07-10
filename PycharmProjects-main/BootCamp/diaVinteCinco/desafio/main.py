45d8c631ade513fcf1986baea34f29a6e8ff2d14
Douglas Luiz dos Santos <douglas.lusan@gmail.com>
2022-07-10 11:44:25 -0300

    desafio dia 25 melhorado com lista dos erros

--- PycharmProjects-main/BootCamp/diaVinteCinco/desafio/main.py
+++ PycharmProjects-main/BootCamp/diaVinteCinco/desafio/main.py
@@ -19,15 +19,23 @@ coord = pd.read_csv('coordenadas.csv')
 fd = pd.DataFrame(coord)
 cont = 0
 completo = 26
+acertos = []
+estudar = []
 
 
 while cont != completo:
 	answer = screen.textinput(title=f'{cont}/{completo}', prompt='Qual o Estado que Falta?').lower()
+	if answer == 'sair':
+		for estado in fd.estado:
+			if estado not in acertos:
+				estudar.append(estado)
+		estudar_df = pd.DataFrame(estudar)
+		estudar_df.to_csv('estudar.csv')
+		break
 	for estado in fd.estado:
-		if estado == answer:
+		if answer == estado:
 			coord = fd[fd.estado == answer]
 			ponto.goto(int(coord.x), int(coord.y))
 			ponto.write(f'{answer.title()}', font=("Arial", 12, "bold"))
+			acertos.append(answer)
 			cont += 1
-
-screen.mainloop()
